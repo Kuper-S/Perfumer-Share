a0e6dc3600743b3cc1a5c20c98fb009bb43590f4
Kuper-S <kupkup1992@gmail.com>
2023-04-27 10:59:23 +0300

    add store and provider to main file.

--- client/src/pages/Login-SingIn/SignupPage.jsx
+++ client/src/pages/Login-SingIn/SignupPage.jsx
@@ -2,11 +2,10 @@ import React , {useState} from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { MDBIcon, MDBPopover, MDBPopoverBody } from 'mdbreact';
+import { MDBIcon} from 'mdbreact';
 import {
   MDBContainer,
   MDBInput,
-  MDBCheckbox,
   MDBBtn,
   MDBRow,
   MDBCol,
--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './state/reducers/rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
